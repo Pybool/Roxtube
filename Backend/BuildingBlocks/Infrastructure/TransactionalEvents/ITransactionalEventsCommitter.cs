9b7834f75295f4ea2792faa1a4b83ec9e4a1672d
Pybool <ekoemmanueljavl@gmail.com>
2023-09-23 06:28:54 +0100

    Initial commit

---
+++ Backend/BuildingBlocks/Infrastructure/TransactionalEvents/ITransactionalEventsCommitter.cs
@@ -0,0 +1,9 @@
+﻿using Domain.TransactionalEvents;
+
+namespace Infrastructure.TransactionalEvents {
+    public interface ITransactionalEventsCommitter {
+        Dictionary<string, List<TransactionalEvent>> ObtainEventGroups ();
+        void RemoveFromContext (Dictionary<string, List<TransactionalEvent>> eventGroups);
+        Task AddToContextAsync (Dictionary<string, List<TransactionalEvent>> eventGroups, TimeSpan? availableDelay = null, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Backend/BuildingBlocks/Infrastructure/TransactionalEvents/Processing/ITransactionalEventsHandler.cs
@@ -0,0 +1,7 @@
+﻿using Domain.TransactionalEvents;
+
+namespace Infrastructure.TransactionalEvents.Processing {
+    public interface ITransactionalEventsHandler {
+        Task ProcessTransactionalEventsAsync (List<TransactionalEvent> events, CancellationToken cancellationToken);
+    }
+}
